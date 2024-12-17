b9ea6bbba498322ea000d0a588a81eff026d504d
Wilkenson <wilkensoncode@gmail.com>
2024-12-16 23:32:50 -0500

    add Editot admin

---
+++ admin/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+function Footer() {
+    return (
+      
+      <footer className="bg-white/5 py-12">
+          <div className="container mx-auto text-center">  
+            <p className="text-gray-400">
+  
+              © 2024 Kadeeno. All rights reserved.
+            </p>
+          </div>
+      </footer>
+      
+    )
+  }
+  
+  export default Footer
