6cd4cfeaa597ed751d9e381903587e6e231a508f
Wilkenson <wilkensoncode@gmail.com>
2025-05-24 19:48:10 -0400

    added policy and term to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-white/5 py-12">
+    <footer className="bg-white/5 py-2">
       <div className="container mx-auto text-center">
         <p className="text-gray-400">
           © {currentYear} Kadeeno Pulse. All rights reserved.
+        </p>
+        <p className="home__subscribe-text px-5">
+          By subscribing to this newsletter you agree to the{" "}
+          <Link to="/privacy-policy" className="home__privacy-policy-link">
+            Privacy Policy
+          </Link>
+          and{" "}
+          <Link to="/term-of-use" className="home__term-of-use-link">
+            Term of use
+          </Link>
         </p>
       </div>
     </footer>
